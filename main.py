5bca050627eff42d226e83792b0dddba6622d481
Thomas <thsbrown1@gmail.com>
2021-11-30 17:54:39 +0000

    Update with default path

--- main.py
+++ main.py
@@ -3,7 +3,22 @@ import os
 
 def run():
     default_path = os.getenv("GITHUB_WORKSPACE")
-    print(default_path)
+    project_settings_file_path = default_path + '/ProjectsSettings/ProjectsSettings.asset'
+    unity_document = UnityDocument.load_yaml(project_settings_file_path)
+    projectsettings_monobehaviour_document = unity_document.entry
+    try:
+        version = projectsettings_monobehaviour_document.AndroidBundleVersionCode
+    except AttributeError:
+        print("Failed to find photonNetworkVersion in NetworkConfig.asset")
+        return
+
+    old_networking_version = version
+    version += 1
+    print(str(old_networking_version) + " -> " + str(version))
+    projectsettings_monobehaviour_document.photonNetworkingVersion = version
+    # see https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-output-parameter
+    print(f"::set-output name=new-build-number::{version}")
+    unity_document.dump_yaml()
     return
 
 
